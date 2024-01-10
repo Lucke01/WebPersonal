1c51eccf4d67602c335546204fff1abf3be29458
Lucas Federici <Luckefederici1@gmail.com>
2024-01-10 20:56:02 -0300

    Templates creados y views actualizadas

--- WebPersonal/Core/views.py
+++ WebPersonal/Core/views.py
@@ -1,40 +1,18 @@
 from django.shortcuts import render, HttpResponse
 
-html_base = """
-            <h1>Mi Web Personal</h1>
-            <ul>
-                <li><a href="/">Portada</a></li>
-                <li><a href="/about/">Acerca de</a></li>
-                <li><a href="/contact/">Contacto</a></li>
-
-            </ul>
-            """
-
 
 #definiendo HOME
 def home(request):
-    return HttpResponse(html_base + '<h1> Mi web personal </h1>')
+    return render(request, 'Core/home.html')
 
 
 #definiendo ABOUT
 def about(request):
-    return HttpResponse(html_base + '''
-                        <h1>Mi Web Personal</h1>
-                        <h2>Acerca de</h2>
-                        <p>Me llamo Lucas y practico con Django</p>
-                        
-                        '''
-                        )
+    return render(request ,'Core/about.html')
 
 
 #definiendo CONTACT
-html_contact = '''
-               <h2>Contacto</h2>
-               <p>Les dejo mi repositorio: </p>
-               <ul>
-                    <li><a href="https://github.com/Lucke01">Github</a></li>
-                </ul>
-                '''
+
 
 def contact(request):
-    return HttpResponse(html_base + html_contact)
+    return render(request , 'Core/contact.html')
